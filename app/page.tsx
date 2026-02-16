32fca2514902c1f80ded5743f2cd6ef93a4189a9
Kanepiro <hiro-k@hh.iij4u.or.jp>
2026-02-16 15:59:46 +0900

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -372,7 +372,7 @@ export default function Home() {
       <div className="card">
         <div className="header">
           <div className="title">AI-Stamp</div>
-          <div className="version">v4.0.002</div>
+          <div className="version">v4.0.004</div>
         </div>
 
         <div className="row">
